c848244a815329a0db5ee875a1495825fa1ada99
Alex Shkil <alexhlins@gmail.com>
2022-11-14 18:40:20 +0300

    added interactive messages

--- black_lists_loader.py
+++ black_lists_loader.py
@@ -6,11 +6,18 @@ from os.path import dirname, realpath
 
 def main():
     settings_location = dirname(realpath(__file__)) + '/settings'
+    print(f'Loading settings from {settings_location} ...')
     loaded_vars = load_variables_to_environment(settings_location)
+    print(f'Loaded {len(loaded_vars)} vars')
     blocked_ips = download_blocked_ips(loaded_vars)
+    print(f'Loaded {len(blocked_ips)} blocked ips')
     blocked_domains = download_blocked_domains(loaded_vars)
+    print(f'Loaded {len(blocked_domains)} blocked domains')
     upload_blocked_ips(blocked_ips)
+    print(f'Blocked ips uploaded to db')
     upload_blocked_domains(blocked_domains)
+    print(f'Blocked domains uploaded to db')
+    print(f'Script finished successfully!')
     return 0
 
 
